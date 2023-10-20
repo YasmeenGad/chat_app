a1c41f51caa81c0800ee0f5589f88d62b342d580
yasmeen <yasmynjad53@gmail.com>
2023-10-20 22:06:06 +0300

    create app bar in chat screen

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/constant.dart';
 import 'package:flutter/material.dart';
 
 class Chat extends StatelessWidget {
@@ -6,6 +7,20 @@ class Chat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        title: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset(
+            kLogo,
+            height: 50,
+          ),
+          Text(
+            "Chat",
+            style: TextStyle(),
+          )
+        ]),
+      ),
+    );
   }
 }
